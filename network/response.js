f4c9d2ca26a7b9ef09487f52e2110c7173888db2
Gabi <g.federico.rodriguez@gmail.com>
2021-02-13 14:56:49 -0300

    fake db and controller

--- network/response.js
+++ network/response.js
@@ -1,3 +1,4 @@
+//respuesta positiva a la peticion
 exports.success = (req, res, message, status) => {
   let statusCode = status || 200; 
   let statusMessage = message || '';
@@ -9,6 +10,7 @@ exports.success = (req, res, message, status) => {
   });
 }
 
+//respuesta negativa a la peticion
 exports.error = (req, res, message, status) => {
   let statusCode = status || 500; 
   let statusMessage = message || 'Internal server error';
---
+++ store/dummy.js
@@ -0,0 +1,28 @@
+const db = {
+  user: [{ id: 1, name: "Gabriel" }],
+};
+
+const list = (table) => {
+  return db[table];
+};
+
+const get = (table, id) => {
+  let collection = list(table);
+
+  return collection.filter((item) => item.id === id)[0] || null;
+};
+
+const upsert = (table, data) => {
+  db[collection].push(data);
+};
+
+const remove = (table, id) => {
+  return true;
+};
+
+module.exports = {
+  list,
+  get,
+  upsert,
+  remove,
+};
