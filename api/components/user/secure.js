2d1fa86c4cf2969fbab240de2fe1d00a9f9a7271
Gabi <g.federico.rodriguez@gmail.com>
2021-02-14 14:22:10 -0300

    middleware permission management

---
+++ api/components/user/secure.js
@@ -0,0 +1,21 @@
+const auth = require('../../../auth');
+
+//funcion que gestiona los permisos de la app
+module.exports = function checkAuth(action){
+
+  //recibe la accion que querramos hacer
+  //y ejecuta el método correspondiente
+  const middleware = (req, res, next) => {
+    switch(action){
+      case 'update':
+        const owner = req.body.id;
+        auth.check.own(req, owner);
+        break;
+      
+      default:
+        next();
+    }
+  }
+
+  return middleware;
+};
--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,51 @@
 const jwt = require("jsonwebtoken");
+const config = require('../config');
+const secret = config.jwt.secret;
 
+//funcion para logearse
 const sign = (data) => {
-  return jwt.sign(data, 'secreto');
+  return jwt.sign(data, secret);
 };
+
+//funcion que recibe el token y lo decodifica
+const verify = (token) => {
+  return jwt.verify(token, secret)
+}
+
+const check = { //Objeto que guarda las funciones de check in
+  own: function(req, owner){
+    const decoded = decodeHeader(req);
+    console.log(decoded);
+
+    //COMPROBAR SI ES O NO PROPIO
+  }
+}
+
+//funcion para obtener el token limpio
+const getToken = (auth) => {
+  if (!auth) {
+    throw new Error('No viene el token');
+  }
+  if (auth.indexOf('Bearer ') == -1) {
+    throw new Error('Formato inválido')
+  }
+  let token = auth.replace('Bearer ', '');
+
+  return token;
+}
+
+//funcion que decodifica el token
+const decodeHeader = (req) => {
+  const authorization = req.headers.authorization || '';
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded;
+
+  return decoded;
+}
 
 module.exports = {
   sign,
+  check
 }
