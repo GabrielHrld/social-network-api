655b7eeab68a4e6cb7f4853c5c6d9f68155fcd33
Gabi <g.federico.rodriguez@gmail.com>
2021-02-16 14:54:38 -0300

    envs

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,4 +1,13 @@
-const store = require('../../../store/remote-mysql');
+// const store = require('../../../store/remote-mysql');
+
+const config = require('../../../config');
 const controller = require('./controller');
+
+let store;
+if (config.remoteDB === true) {
+  store = require('../../../store/remote-mysql');
+} else {
+  store = require('../../../store/mysql');
+}
 
 module.exports = controller(store);
--- config.js
+++ config.js
@@ -1,23 +1,24 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 module.exports = {
+  remoteDB: process.env.REMOTE_DB || false,
   api: {
     port: process.env.API_PORT || 3000,
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'notasecret!',
+    secret: process.env.JWT_SECRET || "",
   },
   mysql: {
-    host: process.env.MYSQL_HOST || 'localhost',
-    user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'crazylife013',
-    database: process.env.MYSQL_DATABASE || 'social-network',
+    host: process.env.MYSQL_HOST || "",
+    user: process.env.MYSQL_USER || "",
+    password: process.env.MYSQL_PASSWORD || "",
+    database: process.env.MYSQL_DATABASE || "",
   },
   mysqlService: {
-    port : process.env.MYSQL_SRV_PORT || 3001,
-    host: process.env.MYSQL_SRV_HOST || 'localhost'
+    port: process.env.MYSQL_SRV_PORT || 3001,
+    host: process.env.MYSQL_SRV_HOST || "",
   },
   post: {
     port: process.env.POST_PORT || 3002,
-  }
-}
+  },
+};
